472a8ec4901455dc4a8df3f4716877b452297993
yoowonsuk <52575749+yoowonsuk@users.noreply.github.com>
2019-09-12 16:07:56 +0900

    Update helloworld.ml

--- helloworld.ml
+++ helloworld.ml
@@ -2,3 +2,19 @@ let hello = "hello";;
 let world = "world";;
 let helloworld = hello ^ " " ^ world;;
 let _ = print_endline helloworld;;
+
+1+2*3;;
+
+7/3;;
+
+3 = 3;;
+4 <> 4;;
+
+3 + int_of_float 2.0;; (* cannot express different types of value *)
+1.2 +. 3.5;;
+
+'c';;
+"cose212";;
+();;
+
+(* neg (-1);; *)
